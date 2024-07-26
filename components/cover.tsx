5d965d571ea1a327a1a46b1918c769499efcc2e0
Swetabh333 <swetabh333@gmail.com>
2024-07-26 11:41:27 +0530

    fixing build err

--- components/cover.tsx
+++ components/cover.tsx
@@ -57,7 +57,7 @@ export const Cover =({
     )
 }
 
-Cover.Skeleton = function COverSkeleton(){
+Cover.Skeleton = function CoverSkeleton(){
     return(
         <Skeleton  className="w-full h-[12vh]"/>
     )
